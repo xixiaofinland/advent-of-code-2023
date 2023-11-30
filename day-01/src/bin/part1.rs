a39c43ede77d33d5ee4595972ba92676ebdf72f7
xi xiao <tdxiaoxi2@gmail.com>
2023-11-30 17:08:07 +0200

    +

--- day-01/src/bin/part1.rs
+++ day-01/src/bin/part1.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, part1!");
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+fn part1(input: &str) -> String {
+    input.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = include_str!("./input1.txt");
+        let result = part1(input);
+        assert_eq!(result, "4\n".to_string());
+    }
 }
