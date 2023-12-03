d295de7c72a19dfdf23771857ba5824ae61e4203
xi xiao <tdxiaoxi2@gmail.com>
2023-12-03 20:00:41 +0200

    day03 part2

--- day03/src/bin/part2.rs
+++ day03/src/bin/part2.rs
@@ -1,96 +1,145 @@
 use std::fs;
 
+#[derive(Debug)]
+struct Num {
+    line: usize,
+    value: usize,
+    start: usize,
+    end: usize,
+}
+
 fn main() {
-    let mut result: u32 = 0;
-    let input = fs::read_to_string("./input1.txt").unwrap();
-    for line in input.lines() {
-        result += parse2(&parse(line));
-    }
+    let input = fs::read_to_string("./i1.txt").unwrap();
+    let result = do_it(&input);
     dbg!(result);
 }
 
-fn parse(input: &str) -> String {
-    let mut result = input.to_string();
-    let elements: [(&str, &str); 17] = [
-        ("oneight", "18"),
-        ("twone", "21"),
-        ("threeight", "38"),
-        ("fiveight", "58"),
-        ("sevenine", "79"),
-        ("eightwo", "82"),
-        ("eighthree", "83"),
-        ("nineight", "98"),
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-    ];
+fn do_it(input: &str) -> usize {
+    let mut symbol_result: Vec<(i32, i32)> = vec![];
+    let mut num_result: Vec<Num> = vec![];
 
-    for el in &elements {
-        result = result.replace(el.0, el.1);
+    for (i, line_data) in input.lines().enumerate() {
+        let t = process_line(i, line_data);
+        num_result.extend(t.0);
+        symbol_result.extend(t.1);
     }
 
+    let mut result = 0;
+    for s in symbol_result {
+        result += get_gear_result(s, &num_result);
+    }
     result
 }
 
-fn parse2(input: &str) -> u32 {
-    let nums: Vec<_> = input.chars().filter_map(|c| c.to_digit(10)).collect();
+fn get_gear_result(s: (i32, i32), num_result: &Vec<Num>) -> usize {
+    let mut values: Vec<usize> = vec![];
 
-    if nums.len() == 1 {
-        nums[0] * 11
-    } else {
-        nums[0] * 10 + nums[nums.len() - 1]
+    // left
+    let left_value = get_value((s.0, s.1 - 1), num_result);
+    if left_value != 0 {
+        values.push(left_value);
     }
+
+    // right
+    let right_value = get_value((s.0, s.1 + 1), num_result);
+    if right_value != 0 {
+        values.push(right_value);
+    }
+
+    // up; if no up left; then up right;
+    let up_result = get_value((s.0 - 1, s.1), num_result);
+    if up_result == 0 {
+        let up_left_result = get_value((s.0 - 1, s.1 - 1), num_result);
+        if up_left_result != 0 {
+            values.push(up_left_result);
+        }
+        let up_right_result = get_value((s.0 - 1, s.1 + 1), num_result);
+        if up_right_result != 0 {
+            values.push(up_right_result);
+        }
+    } else {
+        values.push(up_result)
+    }
+
+    // down; if no down left; then down right;
+    let down_result = get_value((s.0 + 1, s.1), num_result);
+    if down_result == 0 {
+        let down_left_result = get_value((s.0 + 1, s.1 - 1), num_result);
+        if down_left_result != 0 {
+            values.push(down_left_result);
+        }
+        let down_right_result = get_value((s.0 + 1, s.1 + 1), num_result);
+        if down_right_result != 0 {
+            values.push(down_right_result);
+        }
+    } else {
+        values.push(down_result)
+    }
+
+    dbg!(&values);
+
+    if values.len() == 2 {
+        return values[0] * values[1];
+    }
+    0
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn get_value(location: (i32, i32), num_result: &Vec<Num>) -> usize {
+    for n in num_result {
+        if location.0 == n.line.try_into().unwrap()
+            && location.1 >= n.start.try_into().unwrap()
+            && location.1 <= n.end.try_into().unwrap()
+        {
+            return n.value;
+        }
+    }
+    0
+}
 
-    #[test]
-    fn t1() {
-        let result = parse2(&parse("two1nine"));
-        assert_eq!(result, 29);
+fn process_line(line: usize, line_data: &str) -> (Vec<Num>, Vec<(i32, i32)>) {
+    let mut num_result: Vec<Num> = vec![];
+    let mut symbol_result: Vec<(i32, i32)> = vec![];
+    let mut num_in_string = String::new();
+    let mut is_num_start = true;
+    let mut start = 0;
+
+    let end = line_data.chars().count() - 1;
+    let indices = line_data.char_indices();
+    for (i, ch) in indices {
+        if ch == '*' {
+            symbol_result.push((line.try_into().unwrap(), i.try_into().unwrap()));
+        }
+
+        if ch.is_digit(10) {
+            if is_num_start {
+                is_num_start = false;
+                start = i;
+            }
+            num_in_string.push(ch);
+
+            if i == end {
+                num_result.push(Num {
+                    line,
+                    value: num_in_string.parse::<usize>().unwrap(),
+                    start,
+                    end: i - 1,
+                });
+            }
+        } else {
+            if !num_in_string.is_empty() {
+                num_result.push(Num {
+                    line,
+                    value: num_in_string.parse::<usize>().unwrap(),
+                    start,
+                    end: i - 1,
+                });
+            }
+
+            // reset status
+            is_num_start = true;
+            num_in_string.clear();
+        }
     }
 
-    #[test]
-    fn t2() {
-        let result = parse2(&parse("eightwothree"));
-        assert_eq!(result, 83);
-    }
-
-    #[test]
-    fn t3() {
-        let result = parse2(&parse("abcone2threexyz"));
-        assert_eq!(result, 13);
-    }
-
-    #[test]
-    fn t4() {
-        let result = parse2(&parse("xtwone3four"));
-        assert_eq!(result, 24);
-    }
-
-    #[test]
-    fn t5() {
-        let result = parse2(&parse("4nineeightseven2"));
-        assert_eq!(result, 42);
-    }
-
-    #[test]
-    fn t6() {
-        let result = parse2(&parse("zoneight234"));
-        assert_eq!(result, 14);
-    }
-
-    #[test]
-    fn t7() {
-        let result = parse2(&parse("7pqrstsixteen"));
-        assert_eq!(result, 76);
-    }
+    (num_result, symbol_result)
 }
