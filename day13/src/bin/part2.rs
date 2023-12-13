faf2a6e13f8d0970e03e23c05b05a67b036a0843
xi xiao <tdxiaoxi2@gmail.com>
2023-12-13 22:46:18 +0200

    p2 middle

--- day13/src/bin/part2.rs
+++ day13/src/bin/part2.rs
@@ -1,84 +1,141 @@
 use std::fs;
 
-// todo
-const WIDTH: usize = 140;
-const HIGHT: usize = 140;
-const TIMES: usize = 1000000;
-
 fn main() {
     let input = fs::read_to_string("./input.txt").unwrap();
-    let (map, galaxies) = parse(&input);
-    let (empty_column, empty_row) = cal(map);
-    let result = exec(galaxies, empty_column, empty_row);
-    dbg!(result);
+    let r = parse(&input);
+    dbg!(r);
 }
 
-fn parse(input: &str) -> ([[char; WIDTH]; HIGHT], Vec<(usize, usize)>) {
-    let mut map = [['0'; WIDTH]; HIGHT];
-    let mut galaxies: Vec<(usize, usize)> = Vec::new();
-
-    for (i, l) in input.lines().enumerate() {
-        for (j, c) in l.chars().enumerate() {
-            map[i][j] = c;
-            if c == '#' {
-                galaxies.push((i, j));
-            }
-        }
-    }
-    (map, galaxies)
-}
-
-fn cal(map: [[char; WIDTH]; HIGHT]) -> (Vec<usize>, Vec<usize>) {
-    let mut empty_column: Vec<usize> = Vec::new();
-    let mut empty_row: Vec<usize> = Vec::new();
-
-    for i in 0..HIGHT {
-        for j in 0..WIDTH {
-            if map[i][j] == '#' {
-                break;
-            }
-            if j == WIDTH - 1 {
-                empty_row.push(i);
-            }
-        }
-    }
-
-    for i in 0..WIDTH {
-        for j in 0..HIGHT {
-            if map[j][i] == '#' {
-                break;
-            }
-            if j == HIGHT - 1 {
-                empty_column.push(i);
-            }
-        }
-    }
-
-    (empty_column, empty_row)
-}
-
-fn exec(galaxies: Vec<(usize, usize)>, empty_column: Vec<usize>, empty_row: Vec<usize>) -> usize {
+fn parse(input: &str) -> usize {
     let mut result = 0;
-    let end_element = galaxies.len();
-    for i in 0..end_element - 1 {
-        for j in i + 1..end_element {
-            let (a, b) = galaxies[i];
-            let (c, d) = galaxies[j];
-            for step in a.min(c) + 1..a.max(c) + 1 {
-                if empty_row.contains(&step) {
-                    result += TIMES;
-                } else {
-                    result += 1;
-                }
+    let maps: Vec<&str> = input.split("\n\n").collect();
+    let mut lines: Vec<Vec<char>> = Vec::new();
+    let mut columns: Vec<Vec<char>> = Vec::new();
+    let mut line: Vec<char> = Vec::new();
+    let mut column: Vec<char> = Vec::new();
+
+    for map in maps {
+        for l in map.lines() {
+            for c in l.chars() {
+                line.push(c);
             }
-            for step in b.min(d) + 1..b.max(d) + 1 {
-                if empty_column.contains(&step) {
-                    result += TIMES;
-                } else {
-                    result += 1;
-                }
-            }
+            lines.push(line.clone());
+            line.clear();
         }
+
+        for i in 0..lines[0].len() {
+            for j in 0..lines.len() {
+                column.push(lines[j][i]);
+            }
+            columns.push(column.clone());
+            column.clear();
+        }
+
+        let r = cal(&lines, &columns);
+        result += r;
+        lines.clear();
+        columns.clear();
     }
     result
+}
+
+fn cal(lines: &Vec<Vec<char>>, columns: &Vec<Vec<char>>) -> usize {
+    let mut count = 0;
+    for i in 0..lines.len() - 1 {
+        if i == 0 {
+            if one_node_diff_only(&lines[i], &lines[i + 1]) {
+                count += 100;
+                break;
+            }
+        } else if i == lines.len() - 2 {
+            if one_node_diff_only(&lines[i], &lines[i + 1]) {
+                count += 100 * (i + 1);
+                break;
+            }
+        } else {
+            if lines[i] == lines[i + 1] {
+                let mut found = 0;
+                let mut s: i32 = 1;
+                let mut is_reflect = true;
+                let ii = i32::try_from(i).unwrap();
+                while ii - s >= 0 && ii + 1 + s <= i32::try_from(lines.len()).unwrap() - 1 {
+                    let ss = usize::try_from(s).unwrap();
+                    if lines[i - ss] == lines[i + 1 + ss] {
+                        s += 1;
+                    } else if one_node_diff_only(&lines[i - ss], &lines[i + 1 + ss]) {
+                        s += 1;
+                        found += 1;
+                        if found > 1 {
+                            is_reflect = false;
+                            break;
+                        }
+                    } else {
+                        is_reflect = false;
+                        break;
+                    }
+                }
+                if is_reflect {
+                    dbg!(count);
+                    count += (i + 1) * 100;
+                    break;
+                }
+            } else if one_node_diff_only(&lines[i], &lines[i + 1]) {
+                let mut s: i32 = 1;
+                let mut is_reflect = true;
+                let ii = i32::try_from(i).unwrap();
+                while ii - s >= 0 && ii + 1 + s <= i32::try_from(lines.len()).unwrap() - 1 {
+                    let ss = usize::try_from(s).unwrap();
+                    if lines[i - ss] == lines[i + 1 + ss] {
+                        s += 1;
+                    } else {
+                        is_reflect = false;
+                        break;
+                    }
+                }
+                if is_reflect {
+                    dbg!(count);
+                    count += (i + 1) * 100;
+                }
+            }
+        }
+    }
+
+    // for i in 0..columns.len() - 1 {
+    //     if one_node_diff_only(&columns[i], &columns[i + 1]) {
+    //         let mut s: i32 = 1;
+    //         let mut is_reflect = true;
+    //         let ii = i32::try_from(i).unwrap();
+    //         while ii - s >= 0 && ii + 1 + s <= i32::try_from(columns.len()).unwrap() - 1 {
+    //             let ss = usize::try_from(s).unwrap();
+    //             if columns[i - ss] == columns[i + 1 + ss] {
+    //                 s += 1;
+    //             } else {
+    //                 is_reflect = false;
+    //                 break;
+    //             }
+    //         }
+    //         if is_reflect {
+    //             count += i + 1;
+    //         }
+    //     }
+    // }
+    count
+}
+
+fn one_node_diff_only(a: &Vec<char>, b: &Vec<char>) -> bool {
+    let mut diff = 0;
+    for i in 0..a.len() {
+        if a[i] != b[i] {
+            diff += 1;
+        }
+        if diff > 1 {
+            return false;
+        }
+    }
+
+    if diff == 0 {
+        return false;
+    } else {
+        return true;
+    }
 }
